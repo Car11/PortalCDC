60221fc1ae8fff6a5ba4ae4acec7749c6f0f69de
Car11 <carlos.echc11@gmail.com>
2018-08-19 21:01:04 -0600

    connerr

--- class/Conexion.php
+++ class/Conexion.php
@@ -3,14 +3,14 @@
 class DATA {
     
 	public static $conn;
-    private static $connSql;
     private static $config="";
 
 	private static function ConfiguracionIni(){
         require_once('Globals.php'); 
         if (file_exists('../../../ini/config.ini')) {
             self::$config = parse_ini_file('../../../ini/config.ini',true); 
-        }         
+        }       
+        else throw new Exception('Acceso denegado al Archivo de configuración.',-1);  
     }  
 
     private static function Conectar(){
@@ -21,29 +21,12 @@ class DATA {
                 return self::$conn;
             }
         } catch (PDOException $e) {
-            //require_once("Log.php");  
-            //log::AddD('FATAL', 'Ha ocurrido al Conectar con la base de datos MySQL[01]', $e->getMessage());
-            $_SESSION['errmsg']= 'Problemas de Conexión';
-            header('Location: ../Error.php');
-            exit;
+            throw new Exception($e->getMessage(),$e->getCode());
         }
-    }
-    
-    public static function ConectarSQL(){
-        try {           
-            if(!isset(self::$connSql)) {
-                $config = parse_ini_file('../ini/config.ini'); 
-                self::$connSql = new PDO("odbc:sqlserver", 'dbaadmin', 'dbaadmin'); 
-                return self::$connSql;
-            }
-        } catch (PDOException $e) {
-            require_once("Log.php");  
-            log::AddD('FATAL', 'Ha ocurrido al Conectar con la base de datos SQL[01]', $e->getMessage());
-            //$_SESSION['errmsg']= $e->getMessage();
-            header('Location: ../Error.php');
-            exit;
+        catch(Exception $e){
+            throw new Exception($e->getMessage(),$e->getCode());
         }
-    }    
+    }  
 
     // Ejecuta consulta SQL, $op = true envía los datos en 'crudo', $op=false envía los datos en arreglo (fetch).
     public static function Ejecutar($sql, $param=NULL, $op=false) {
@@ -59,46 +42,14 @@ class DATA {
                     return  $st->fetchAll();
                 else return $st;    
             } else {
-                self::$conn->rollback(); 
-                require_once("Log.php");  
-                log::AddD('ERROR', 'Ha ocurrido al Ejecutar la sentencia SQL[02]', 'code: ' . $st->errorInfo()[1] . ' msg: ' . $st->errorInfo()[2] );
-                return false;
-            }
-            
+                throw new Exception('Error al ejecutar.',00);
+            }            
         } catch (Exception $e) {
-            self::$conn->rollback(); 
-            require_once("Log.php");  
-            log::AddD('ERROR', 'Ha ocurrido al Ejecutar la sentencia SQL', $e->getMessage());
-            //$_SESSION['errmsg']= $e->getMessage();
-            header('Location: ../Error.php');
-            exit;
-        }
-    }
-    
-    public static function EjecutarSQL($sql, $param=NULL, $op=false) {
-        try{
-            //conecta a BD
-            self::ConectarSQL();    
-            $st=self::$connSql->prepare($sql);
-            self::$conn->beginTransaction(); 
-            if($st->execute($param)){
-                self::$conn->commit(); 
-                if(!$op)
-                    return  $st->fetchAll();
-                else return $st;    
-            } else {
+            if(isset(self::$conn))
                 self::$conn->rollback(); 
-                require_once("Log.php");  
-                log::Add('ERROR', 'Ha ocurrido al Ejecutar la sentencia SQL[02]');
-                return false;
-            }
-        } catch (Exception $e) {
-            self::$conn->rollback(); 
-            require_once("Log.php");  
-            log::AddD('ERROR', 'Ha ocurrido al Ejecutar la sentencia SQL', $e->getMessage());
-            //$_SESSION['errmsg']= $e->getMessage();
-            header('Location: ../Error.php');
-            exit;
+            if(isset($st))
+                throw new Exception($st->errorInfo()[2],$st->errorInfo()[1]);
+            else throw new Exception($e->getMessage(),$e->getCode());
         }
     }
     
