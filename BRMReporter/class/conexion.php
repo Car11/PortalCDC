4573d5e1509fe385cdcb76ca6fb02a29c2baa9fb
Car11 <carlos.echc11@gmail.com>
2018-12-12 06:40:46 -0600

    fix21

--- BRMReporter/class/conexion.php
+++ BRMReporter/class/conexion.php
@@ -53,14 +53,14 @@ class DATA {
             if($r)
             {                
                 if($fetch){
-                    //$rows= oci_fetch_array($st, OCI_ASSOC+OCI_RETURN_NULLS);
+                    $rows= oci_fetch_array($st, OCI_ASSOC);
                     // while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_LOBS)) {
                         
                     //     // In a loop, freeing the large variable before the 2nd fetch reduces PHP's peak memory usage
                     //     unset($row);  
                     // }
 
-                    while($row = oci_fetch_array($st_handle, OCI_ASSOC)) {
+                    while($row = oci_fetch_array($st, OCI_ASSOC)) {
                         error_log("[DEBUG]  : ROWS: " . $row['PROM']);
                         //var_dump($row['another_field']);
                     }
